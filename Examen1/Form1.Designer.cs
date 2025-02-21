c789b15513fe798b41ee55eb373e17c3af69853e
Patatapou <migel2013@outlook.com>
2025-02-21 15:27:02 -0600

    Agregar archivos de proyecto.

---
+++ Examen1/Form1.Designer.cs
@@ -0,0 +1,81 @@
+﻿namespace Examen1
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            DGV = new DataGridView();
+            openFileDialog1 = new OpenFileDialog();
+            BTN_CARGAR = new Button();
+            ((System.ComponentModel.ISupportInitialize)DGV).BeginInit();
+            SuspendLayout();
+            // 
+            // DGV
+            // 
+            DGV.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            DGV.Location = new Point(12, 12);
+            DGV.Name = "DGV";
+            DGV.RowHeadersWidth = 51;
+            DGV.Size = new Size(786, 567);
+            DGV.TabIndex = 0;
+            // 
+            // openFileDialog1
+            // 
+            openFileDialog1.AutoUpgradeEnabled = false;
+            openFileDialog1.FileName = "openFileDialog1";
+            openFileDialog1.Filter = "Archivo CSV|*.csv";
+            // 
+            // BTN_CARGAR
+            // 
+            BTN_CARGAR.Location = new Point(309, 585);
+            BTN_CARGAR.Name = "BTN_CARGAR";
+            BTN_CARGAR.Size = new Size(182, 29);
+            BTN_CARGAR.TabIndex = 1;
+            BTN_CARGAR.Text = "CARGAR ARCHIVO";
+            BTN_CARGAR.UseVisualStyleBackColor = true;
+            BTN_CARGAR.Click += BTN_CARGAR_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(810, 626);
+            Controls.Add(BTN_CARGAR);
+            Controls.Add(DGV);
+            Name = "Form1";
+            Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)DGV).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private DataGridView DGV;
+        private OpenFileDialog openFileDialog1;
+        private Button BTN_CARGAR;
+    }
+}
---
+++ Examen1/Form1.cs
@@ -0,0 +1,76 @@
+namespace Examen1
+{
+    public partial class Form1 : Form
+    {
+        string filePath = "";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void BTN_CARGAR_Click(object sender, EventArgs e)
+        {
+            DialogResult resultado;
+            resultado = openFileDialog1.ShowDialog();
+            if (resultado == DialogResult.OK)
+            {
+                filePath = openFileDialog1.FileName;
+                try
+                {
+                    string documento = File.ReadAllText(filePath);
+
+                    string[] fila = documento.Split('\n');
+                    string[] col = fila[0].Split(',');
+
+                    DGV.ColumnCount = col.Length+2;
+                    DGV.RowCount = fila.Length;
+                    for (int i = 0; i < col.Length; i++)
+                    {
+                        DGV.Rows[0].Cells[i].Value = col[i];
+                    }
+                    for (int i = 1; i < (fila.Length) - 1; i++)
+                    {
+                        col = fila[i].Split(',');
+                        for (int j = 0; j < col.Length; j++)
+                        {
+                            DGV.Rows[i].Cells[j].Value = col[j];
+                            string curp = col[0];
+                            edadYsexo(curp);
+                        }
+                    }
+                    //string curp = CURP.Text;
+                    //edadYsexo(curp);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error al abrir el archivo: " + ex.Message);
+                }
+            }
+        }
+
+        public string[] edadYsexo(string curp)
+        {
+            int[] retorno = new int[2];
+
+            string ano = curp[4].ToString() + curp[5].ToString();
+            string mes = curp[6].ToString() + curp[7].ToString();
+            string dia = curp[8].ToString() + curp[9].ToString();
+
+            DateTime fechaNac = DateTime.Parse(dia + "/" + mes + "/" + ano);
+
+            DateTime hoy = DateTime.Today;
+            TimeSpan edadHoras = hoy.Date - fechaNac.Date;
+            DateTime edad = new DateTime() + edadHoras;
+
+            if (hoy.Month < fechaNac.Month)
+            {
+                retorno[1] = ((edad.Year - 1) + " años");
+            }
+            else
+            {
+                retorno[1] = (edad.Year + " años");
+            }
+            return null;
+        }
+    }
+}
